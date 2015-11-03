771362cf7a55b882322ea4f2772f12880810a5a3
drummer0417 <hans@androidappfactory.nl>
2015-11-04 00:07:51 +0100

    Bugje eruit, Log.d ering

--- app/src/main/java/nl/androidappfactory/maplocation/MapsActivity.java
+++ app/src/main/java/nl/androidappfactory/maplocation/MapsActivity.java
@@ -1,7 +1,8 @@
 package nl.androidappfactory.maplocation;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,13 +16,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
 
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        Log.d("Testing Git", "HelloGitHub");
     }
 
 
